56312fb3ced9be3105741c3c69707b9278ed376e
AravindCodesAllDay <aravindsiva2003.in@gmail.com>
2024-03-19 11:40:00 +0530

    main

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,6 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import "./index.css";
-import WelcomePage from "./pages/WelcomePage.jsx";
 import NoPage from "./pages/NoPage.jsx";
 import Login from "./pages/Login.jsx";
 import UpdateProduct from "./pages/Update.jsx";
